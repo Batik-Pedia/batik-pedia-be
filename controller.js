ac07b6e6d0b9b1b34aacba1a0c4ea392df3d2fcb
Lamz16 <andiku0755@gmail.com>
2024-06-02 04:02:23 +0700

    add get data katalog, berita, wisata

--- controller.js
+++ controller.js
@@ -6,3 +6,39 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Succesfully running REST API!", res)
 };
+
+// Get All data katalog batik
+ exports.getAllKatalogBatik = function(req, res){
+    connection.query("SELECT * FROM katalog_batik", function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    })
+ }
+
+
+
+ // Get All Berita
+ exports.getAllBerita = function(req, res){
+    connection.query("SELECT * FROM berita", function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    })
+ }
+
+
+ //Get All Wisata
+ exports.getAllWisata = function(req, res){
+    connection.query("SELECT * FROM wisata", function(error,rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    })
+ }
--- routes.js
+++ routes.js
@@ -2,6 +2,16 @@
 
 module.exports = function(app){
     var jsonku = require("./controller");
+
     app.route('/')
     .get(jsonku.index);
+
+    app.route('/katalog')
+    .get(jsonku.getAllKatalogBatik);
+
+    app.route('/berita')
+    .get(jsonku.getAllBerita);
+
+    app.route('/wisata')
+    .get(jsonku.getAllWisata);
 }
