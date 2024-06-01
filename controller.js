cb3c97ae9f09f054f65b9e3b9593ad946c172051
Lamz16 <andiku0755@gmail.com>
2024-06-02 04:37:18 +0700

    add by id method

--- controller.js
+++ controller.js
@@ -7,38 +7,85 @@ exports.index = function(req, res){
     response.ok("Succesfully running REST API!", res)
 };
 
+/* Katalog  */
+
 // Get All data katalog batik
  exports.getAllKatalogBatik = function(req, res){
     connection.query("SELECT * FROM katalog_batik", function(error, rows, fields){
         if(error){
-            connection.log(error);
+            console.log(error);
         }else{
             response.ok(rows, res);
         }
     })
  }
 
+//  Get data katalog batik by id
+exports.getKatalogBatikId = function(req, res){
+    let id = req.params.id;
+    connection.query("SELECT * FROM katalog_batik WHERE idBatik = ?", [id],function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    })
+ }
 
+ 
+/* Katalog  */
+
+
+/* Berita */
 
  // Get All Berita
  exports.getAllBerita = function(req, res){
     connection.query("SELECT * FROM berita", function(error, rows, fields){
         if(error){
-            connection.log(error);
+            console.log(error);
         }else{
             response.ok(rows,res);
         }
     })
  }
 
+
+//  Get data berita by id
+exports.getBeritaId = function(req, res){
+    let id = req.params.id;
+    connection.query("SELECT * FROM berita WHERE idBerita = ?", [id],function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    })
+ }
+
+ /* Berita  */
+
+ /* Wisata */
 
  //Get All Wisata
  exports.getAllWisata = function(req, res){
     connection.query("SELECT * FROM wisata", function(error,rows, fields){
         if(error){
-            connection.log(error);
+            console.log(error);
         }else{
             response.ok(rows,res);
         }
     })
  }
+
+ exports.getWisataId = function(req, res){
+    let id = req.params.id;
+    connection.query("SELECT * FROM berita WHERE idWisata = ?", [id],function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    })
+ }
+
+ /* Wisata */
--- routes.js
+++ routes.js
@@ -6,12 +6,32 @@ module.exports = function(app){
     app.route('/')
     .get(jsonku.index);
 
+    /* Katalog */
+    
     app.route('/katalog')
     .get(jsonku.getAllKatalogBatik);
+
+    app.route('/katalog/:id')
+    .get(jsonku.getKatalogBatikId);
+
+    /* Katalog */
+
+    /* Berita */
 
     app.route('/berita')
     .get(jsonku.getAllBerita);
 
+    app.route('/berita/:id')
+    .get(jsonku.getBeritaId);
+
+    /* Berita */
+
+    /* Wisata */
     app.route('/wisata')
     .get(jsonku.getAllWisata);
+
+    app.route('/wisata/:id')
+    .get(jsonku.getWisataId);
+
+    /* Wisata */
 }
