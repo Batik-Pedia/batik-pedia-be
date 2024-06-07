6da6a255d44aef22be29a3c9b4894f74e73d0ccd
Lamz16 <andiku0755@gmail.com>
2024-06-07 11:01:32 +0700

    Update get All batik with filter

--- controller.js
+++ controller.js
@@ -9,13 +9,38 @@ exports.index = function(req, res){
 
 const author = "Andi Salam Syahputra"
 const error404 = 'Data not found'
-const error500 ='Internal Server Error'
 
 /* Katalog  */
 
-// Get All data katalog batik
 exports.getAllKatalogBatik = function(req, res){
-    connection.query("SELECT * FROM katalog_batik", function(error, rows, fields){
+    let query = "SELECT * FROM katalog_batik";
+
+    // Membuat variabel untuk menampung kondisi WHERE
+    let conditions = [];
+
+    // Cek apakah ada parameter filterWilayah dan terapkan filter
+    if (req.query.filterWilayah && req.query.filterWilayah !== 'Semua') {
+        const filterWilayah = req.query.filterWilayah;
+        conditions.push(`wilayah='${filterWilayah}'`);
+    }
+
+    if (req.query.filterJenisBatik && req.query.filterJenisBatik !== 'Semua') {
+        const filterJenisBatik = req.query.filterJenisBatik;
+        conditions.push(`jenisBatik='${filterJenisBatik}'`);
+    }
+
+    if (conditions.length > 0) {
+        query += " WHERE " + conditions.join(" AND ");
+    }
+
+
+    if (req.query.sort) {
+        const sortBy = req.query.sort;
+        const order = req.query.order === 'desc' ? 'DESC' : 'ASC';
+        query += ` ORDER BY ${sortBy} ${order}`;
+    }
+
+    connection.query(query, function(error, rows){
         if (error) {
             response.ok(error, res);
         } else {
@@ -29,10 +54,12 @@ exports.getAllKatalogBatik = function(req, res){
     });
 };
 
+
+
 // Get data katalog batik by id
 exports.getKatalogBatikId = function(req, res){
     let id = req.params.id;
-    connection.query("SELECT * FROM katalog_batik WHERE idBatik = ?", [id], function(error, rows, fields){
+    connection.query("SELECT * FROM katalog_batik WHERE idBatik = ?", [id], function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -47,7 +74,7 @@ exports.getKatalogBatikId = function(req, res){
             }else{
                 response.ok(rows[0], res);
             }
-                
+
         }
     });
 };
@@ -64,21 +91,21 @@ exports.addKatalogBatik = function(req, res){
     var jenisBatik = req.body.jenisBatik;
     var wilayah = req.body.wilayah;
 
-    connection.query("INSERT INTO katalog_batik (namaBatik,detailBatik,sejarahBatik,penggunaan,makna,image,lat,lon,jenisBatik,wilayah) VALUES(?,?,?,?,?,?,?,?,?,?)", 
-    [namaBatik,detailBatik,sejarahBatik,penggunaan,makna,image,lat,lon,jenisBatik,wilayah], function(error, rows, fields){
-        if (error) {
-            console.log(error);
-            response.ok(error,res);
-        } else {
-            response.ok(rows, res,201)
-        }
-    });
+    connection.query("INSERT INTO katalog_batik (namaBatik,detailBatik,sejarahBatik,penggunaan,makna,image,lat,lon,jenisBatik,wilayah) VALUES(?,?,?,?,?,?,?,?,?,?)",
+        [namaBatik,detailBatik,sejarahBatik,penggunaan,makna,image,lat,lon,jenisBatik,wilayah], function(error, rows){
+            if (error) {
+                console.log(error);
+                response.ok(error,res);
+            } else {
+                response.ok(rows, res,201)
+            }
+        });
 
 }
 
 exports.deleteKatalogId = function(req, res){
     let id = req.body.idBatik
-    connection.query("DELETE FROM katalog_batik WHERE idBatik =?", [id], function(error, rows, fields){
+    connection.query("DELETE FROM katalog_batik WHERE idBatik =?", [id], function(error){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -94,7 +121,7 @@ exports.deleteKatalogId = function(req, res){
 
 // Get All Berita
 exports.getAllBerita = function(req, res){
-    connection.query("SELECT * FROM berita", function(error, rows, fields){
+    connection.query("SELECT * FROM berita", function(error, rows){
         if (error) {
             response.ok(error, res);
         } else {
@@ -111,7 +138,7 @@ exports.getAllBerita = function(req, res){
 // Get data berita by id
 exports.getBeritaId = function(req, res){
     let id = req.params.id;
-    connection.query("SELECT * FROM berita WHERE idBerita = ?", [id], function(error, rows, fields){
+    connection.query("SELECT * FROM berita WHERE idBerita = ?", [id], function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -126,39 +153,39 @@ exports.getBeritaId = function(req, res){
             }else{
                 response.ok(rows[0], res);
             }
-                
+
         }
     });
 };
 
 exports.addBerita = function(req, res){
-   var namaBerita = req.body.namaBerita
-   var tglBerita = req.body.tglBerita
-   var lokasiBerita = req.body.lokasiBerita
-   var urlBerita = req.body.urlBerita
-   var imageBerita = req.body.imageBerita
+    var namaBerita = req.body.namaBerita
+    var tglBerita = req.body.tglBerita
+    var lokasiBerita = req.body.lokasiBerita
+    var urlBerita = req.body.urlBerita
+    var imageBerita = req.body.imageBerita
 
-    connection.query("INSERT INTO berita (namaBerita, tglBerita, lokasiBerita, urlBerita, imageBerita) VALUES(?,?,?,?,?)", 
-    [namaBerita, tglBerita, lokasiBerita, urlBerita,imageBerita], function(error, rows, fields){
-        if (error) {
-            console.log(error);
-            response.ok(error,res);
-        } else {
+    connection.query("INSERT INTO berita (namaBerita, tglBerita, lokasiBerita, urlBerita, imageBerita) VALUES(?,?,?,?,?)",
+        [namaBerita, tglBerita, lokasiBerita, urlBerita,imageBerita], function(error, rows){
+            if (error) {
+                console.log(error);
+                response.ok(error,res);
+            } else {
 
-            response.ok(rows, res,201)
-        }
-    });
+                response.ok(rows, res,201)
+            }
+        });
 
 }
 
 exports.deleteBeritaId = function(req, res){
     let id = req.body.idBerita
-    connection.query("DELETE FROM berita WHERE idBerita =?", [id], function(error, rows, fields){
+    connection.query("DELETE FROM berita WHERE idBerita =?", [id], function(error){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-                response.ok("Succesfuly delete", res);
+            response.ok("Succesfuly delete", res);
         }
     });
 };
@@ -169,7 +196,7 @@ exports.deleteBeritaId = function(req, res){
 
 // Get All Wisata
 exports.getAllWisata = function(req, res){
-    connection.query("SELECT * FROM wisata", function(error, rows, fields){
+    connection.query("SELECT * FROM wisata", function(error, rows){
         if (error) {
             response.ok(error, res);
         } else {
@@ -186,7 +213,7 @@ exports.getAllWisata = function(req, res){
 // Get data wisata by id
 exports.getWisataId = function(req, res){
     let id = req.params.id;
-    connection.query("SELECT * FROM wisata WHERE idWisata = ?", [id], function(error, rows, fields){
+    connection.query("SELECT * FROM wisata WHERE idWisata = ?", [id], function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -201,39 +228,39 @@ exports.getWisataId = function(req, res){
             }else{
                 response.ok(rows[0], res);
             }
-                
+
         }
     });
 };
 
 
 exports.addWisata = function(req, res){
-  var namaWisata = req.body.namaWisata
-  var detailWisata = req.body.detailWisata
-  var lat = req.body.lat
-  var lon = req.body.lon
-  var imageWisata = req.body.imageWisata
+    var namaWisata = req.body.namaWisata
+    var detailWisata = req.body.detailWisata
+    var lat = req.body.lat
+    var lon = req.body.lon
+    var imageWisata = req.body.imageWisata
 
-     connection.query("INSERT INTO wisata (namaWisata, detailWisata, lat, lon,imageWisata) VALUES(?,?,?,?,?)", 
-     [namaWisata, detailWisata,lat,lon,imageWisata], function(error, rows, fields){
-         if (error) {
-            console.log(error);
-            response.ok(error,res);
-         } else {
-             response.ok(rows, res,201)
-         }
-     });
- 
- }
+    connection.query("INSERT INTO wisata (namaWisata, detailWisata, lat, lon,imageWisata) VALUES(?,?,?,?,?)",
+        [namaWisata, detailWisata,lat,lon,imageWisata], function(error, rows){
+            if (error) {
+                console.log(error);
+                response.ok(error,res);
+            } else {
+                response.ok(rows, res,201)
+            }
+        });
 
- exports.deleteWisataId = function(req, res){
+}
+
+exports.deleteWisataId = function(req, res){
     let id = req.body.idWisata
-    connection.query("DELETE FROM wisata WHERE idWisata =?", [id], function(error, rows, fields){
+    connection.query("DELETE FROM wisata WHERE idWisata =?", [id], function(error){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-                response.ok("Succesfuly delete", res);
+            response.ok("Succesfuly delete", res);
         }
     });
 };
@@ -246,7 +273,7 @@ exports.addWisata = function(req, res){
 
 // Get All Kursus
 exports.getAllKursus = function(req, res){
-    connection.query("SELECT * FROM kursus_membatik", function(error, rows, fields){
+    connection.query("SELECT * FROM kursus_membatik", function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -266,7 +293,7 @@ exports.getAllKursus = function(req, res){
 // Get data kursus by id
 exports.getKursusId = function(req, res){
     let id = req.params.id;
-    connection.query("SELECT * FROM kursus_membatik WHERE idKursus = ?", [id], function(error, rows, fields){
+    connection.query("SELECT * FROM kursus_membatik WHERE idKursus = ?", [id], function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -281,40 +308,40 @@ exports.getKursusId = function(req, res){
             }else{
                 response.ok(rows[0], res);
             }
-                
+
         }
     });
 };
 
 
 exports.addKursus = function(req, res){
-  var namaKursus = req.body.namaKursus
-  var image = req.body.image
-  var harga = req.body.harga
-  var deskripsi = req.body.deskripsi
-  var urlKursus = req.body.urlKursus
+    var namaKursus = req.body.namaKursus
+    var image = req.body.image
+    var harga = req.body.harga
+    var deskripsi = req.body.deskripsi
+    var urlKursus = req.body.urlKursus
 
-     connection.query("INSERT INTO kursus_membatik (namaKursus, image, harga, deskripsi,urlKursus) VALUES(?,?,?,?,?)", 
-     [namaKursus,image,harga,deskripsi,urlKursus], function(error, rows, fields){
-         if (error) {
-            console.log(error);
-            response.ok(error,res);
-         } else {
- 
-             response.ok(rows, res,201)
-         }
-     });
- 
- }
+    connection.query("INSERT INTO kursus_membatik (namaKursus, image, harga, deskripsi,urlKursus) VALUES(?,?,?,?,?)",
+        [namaKursus,image,harga,deskripsi,urlKursus], function(error, rows){
+            if (error) {
+                console.log(error);
+                response.ok(error,res);
+            } else {
 
- exports.deleteKursusId = function(req, res){
+                response.ok(rows, res,201)
+            }
+        });
+
+}
+
+exports.deleteKursusId = function(req, res){
     let id = req.body.idKursus
-    connection.query("DELETE FROM kursus_membatik WHERE idKursus =?", [id], function(error, rows, fields){
+    connection.query("DELETE FROM kursus_membatik WHERE idKursus =?", [id], function(error){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-                response.ok("Succesfuly delete", res);
+            response.ok("Succesfuly delete", res);
         }
     });
 };
@@ -327,16 +354,12 @@ exports.addKursus = function(req, res){
 
 // Get All Kursus
 exports.getAllVideo = function(req, res){
-    connection.query("SELECT * FROM video_batik", function(error, rows, fields){
+    connection.query("SELECT * FROM video_batik", function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-            var data = {
-                status : 200,
-                author: author,
-                video: rows
-            };
+
 
             response.ok(rows, res);
         }
@@ -346,7 +369,7 @@ exports.getAllVideo = function(req, res){
 // Get data kursus by id
 exports.getVidBatikId = function(req, res){
     let id = req.params.id;
-    connection.query("SELECT * FROM video_batik WHERE idKursus = ?", [id], function(error, rows, fields){
+    connection.query("SELECT * FROM video_batik WHERE idKursus = ?", [id], function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -361,38 +384,38 @@ exports.getVidBatikId = function(req, res){
             }else{
                 response.ok(rows[0], res);
             }
-                
+
         }
     });
 };
 
 
 exports.addVideoBatik = function(req, res){
-  var judulVideo = req.body.judulVideo
-  var imgVideo = req.body.imgVideo
-  var urlVideo = req.body.urlVideo
+    var judulVideo = req.body.judulVideo
+    var imgVideo = req.body.imgVideo
+    var urlVideo = req.body.urlVideo
 
-     connection.query("INSERT INTO video_batik (judulVideo,imgVideo,urlVideo) VALUES(?,?,?)", 
-     [judulVideo,imgVideo,urlVideo], function(error, rows, fields){
-         if (error) {
-            console.log(error);
-            response.ok(error,res);
-         } else {
- 
-             response.ok(rows, res,201)
-         }
-     });
- 
- }
+    connection.query("INSERT INTO video_batik (judulVideo,imgVideo,urlVideo) VALUES(?,?,?)",
+        [judulVideo,imgVideo,urlVideo], function(error, rows){
+            if (error) {
+                console.log(error);
+                response.ok(error,res);
+            } else {
 
- exports.deleteVideoBatik = function(req, res){
+                response.ok(rows, res,201)
+            }
+        });
+
+}
+
+exports.deleteVideoBatik = function(req, res){
     let id = req.body.idVideo
-    connection.query("DELETE FROM video_batik WHERE idKursus =?", [id], function(error, rows, fields){
+    connection.query("DELETE FROM video_batik WHERE idKursus =?", [id], function(error){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-                response.ok("Succesfuly delete", res);
+            response.ok("Succesfuly delete", res);
         }
     });
 };
@@ -405,16 +428,12 @@ exports.addVideoBatik = function(req, res){
 
 // Get All Nusantara
 exports.getAllProvinsi = function(req, res){
-    connection.query("SELECT * FROM provinsi", function(error, rows, fields){
+    connection.query("SELECT * FROM provinsi", function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-            var data = {
-                status : 200,
-                author: author,
-                video: rows
-            };
+
 
             response.ok(rows, res);
         }
@@ -424,7 +443,7 @@ exports.getAllProvinsi = function(req, res){
 // Get data provinsi by id
 exports.getProvinsiId = function(req, res){
     let id = req.params.id;
-    connection.query("SELECT * FROM provinsi WHERE idProvinsi = ?", [id], function(error, rows, fields){
+    connection.query("SELECT * FROM provinsi WHERE idProvinsi = ?", [id], function(error, rows){
         if (error) {
             console.log(error);
             response.ok(error,res);
@@ -439,38 +458,38 @@ exports.getProvinsiId = function(req, res){
             }else{
                 response.ok(rows[0], res);
             }
-                
+
         }
     });
 };
 
 
 exports.addProvinsi = function(req, res){
-  var namaProvinsi = req.body.namaProvinsi
-  var imgProvinsi = req.body.imgProvinsi
-  var detailProvinsi = req.body.detailProvinsi
+    var namaProvinsi = req.body.namaProvinsi
+    var imgProvinsi = req.body.imgProvinsi
+    var detailProvinsi = req.body.detailProvinsi
 
-     connection.query("INSERT INTO provinsi (namaProvinsi,imgProvinsi,detailProvinsi) VALUES(?,?,?)", 
-     [namaProvinsi,imgProvinsi,detailProvinsi], function(error, rows, fields){
-         if (error) {
-            console.log(error);
-            response.ok(error,res);
-         } else {
- 
-             response.ok(rows, res,201)
-         }
-     });
- 
- }
+    connection.query("INSERT INTO provinsi (namaProvinsi,imgProvinsi,detailProvinsi) VALUES(?,?,?)",
+        [namaProvinsi,imgProvinsi,detailProvinsi], function(error, rows){
+            if (error) {
+                console.log(error);
+                response.ok(error,res);
+            } else {
 
- exports.deleteProvinsi = function(req, res){
+                response.ok(rows, res,201)
+            }
+        });
+
+}
+
+exports.deleteProvinsi = function(req, res){
     let id = req.body.idProvinsi
-    connection.query("DELETE FROM provinsi WHERE idKursus =?", [id], function(error, rows, fields){
+    connection.query("DELETE FROM provinsi WHERE idKursus =?", [id], function(error){
         if (error) {
             console.log(error);
             response.ok(error,res);
         } else {
-                response.ok("Succesfuly delete", res);
+            response.ok("Succesfuly delete", res);
         }
     });
 };
